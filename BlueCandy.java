ef98333c6441c75ba1a2d211e6cbba29957f6670
sophia-jli <sophiali8080@gmail.com>
2024-05-21 11:51:47 -0400

    Initial files

---
+++ BlueCandy.java
@@ -0,0 +1,25 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class BlueCandy here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class BlueCandy extends Candy
+{
+    /**
+     * Act - do whatever the BlueCandy wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public BlueCandy() {
+        GreenfootImage image = getImage();
+        image.scale(25 , 25);
+        setImage(image);
+    }
+    
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
